48affb0abc01595ce5e127180d36585c66af8afd
Dakota Schramm <66275302+Dakota-Schramm@users.noreply.github.com>
2021-10-24 16:34:45 -0400

    Add files via upload

---
+++ fcc-timestamp-microservice/server.js
@@ -0,0 +1,54 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+const e = require('express');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+// ADD OTHER ENDPOINTS HERE
+// Endpoint for ?date
+app.get("/api/:date", (req, res) => {
+  // If date passed is Unix time
+  if ((req.params.date).match(/^[0-9]+$/) != null) {
+    req.params.date *= 1000
+  }
+  const date = new Date(req.params.date);
+  console.log(date);
+  if (date.toString() !== 'Invalid Date') {
+    res.json({
+      unix: Math.floor(date.getTime() / 1000),
+      utc: date.toUTCString()
+    });
+  } else {
+    res.json({
+      error: date.toString()
+    })
+  }
+})
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
